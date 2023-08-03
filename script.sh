0eec84fa6c7a50b6fdd6ca0b6a6983a8b1fe582c
Pesar <pesar369@gmail.com>
2023-08-03 20:19:34 +0330

    Fixed problems

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ readarray -t domains < /etc/domain-detective/domains.txt
 
 function check_domain {
   domain=$1
-  status=$(whois $domain | grep -E '\b(?:no entries found|No match for domain|Domain not found|No Data Found|DOMAIN NOT FOUND)\b')
+  status=$(whois $domain | grep -E '\b(?:404|No match for domain|Domain not found|No Data Found|DOMAIN NOT FOUND|no entries found)\b')
   if [[ -n "$status" ]]; then
     echo "domain $domain is available!"
   fi
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-mkdir -p /etc/domain-detective && touch /etc/domain-detective/domains.txt
+mkdir -p /etc/domain-detective
+touch /etc/domain-detective/domains.txt
+echo -e "test.ir\nexample.com\ndomain.org" > /etc/domain-detective/domains.txt
