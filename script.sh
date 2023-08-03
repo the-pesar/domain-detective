488a8250a671d7ad62b4fbe0f1be78b616802ffb
Pesar <pesar369@gmail.com>
2023-08-03 17:05:17 +0330

    Support for new suffixes

--- script.sh
+++ script.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+# replace your domains
 domains=(
   example.ir
-  test.ir
-  domain.ir
-  just-for-test.ir
+  just-for-test.com
+  domain.org
+  just-for-test.shop
+  just-for-test.me
+  test.info
+  world.co
 )
 
 function check_domain {
   domain=$1
-  status=$(whois $domain | grep 'no entries found')
+  status=$(whois $domain | grep -E '\b(?:no entries found|No match for domain|Domain not found|No Data Found|DOMAIN NOT FOUND)\b')
   if [[ -n "$status" ]]; then
-    echo "domain: $domain is free!"
+    echo "domain $domain is available!"
   fi
 }
 
